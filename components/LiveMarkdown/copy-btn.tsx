2d4202bd3cf1ddee9f67927954ece27017a9fc1f
zhenjie <zhenjiezou@tencent.com>
2023-01-27 20:20:47 +0800

    feat: add successfully copied message for copy button
    add useMessage hook

--- components/LiveMarkdown/copy-btn.tsx
+++ components/LiveMarkdown/copy-btn.tsx
@@ -1,4 +1,5 @@
 import ContentCopyOutlined from "@mui/icons-material/ContentCopyOutlined"
+import { useMessage } from "common/hooks/useMessage";
 import { Children, SyntheticEvent } from "react"
 
 interface Props {
@@ -6,11 +7,17 @@ interface Props {
 }
 
 export const CopyBtn: React.FC<Props> = ({ children }) => {
+  const { Message, setOpen } = useMessage('Copied Successfully!')
   const handleClick = (e: SyntheticEvent) => {
     navigator.clipboard.writeText((Children.toArray(children)[0] as JSX.Element).props.children[0])
-    // TODO: show message tips: Copied
+    setOpen(true)
   }
-  return <span className="text-white absolute right-2 top-2 transition cursor-pointer hover:scale-110">
-    <ContentCopyOutlined onClick={handleClick} />
-  </span>
+  return (
+    <>
+      {Message}
+      <span className="text-white absolute right-2 top-2 transition cursor-pointer hover:scale-110">
+        <ContentCopyOutlined onClick={handleClick} />
+      </span>
+    </>
+  )
 }
