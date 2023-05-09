05ccbee36b2dbd0d205b032866aebb5564cab97b
Joey Z <63656376+Joey-J3@users.noreply.github.com>
2023-05-09 23:50:52 +0800

    feat(live-markdown): transform pasted content into markdown format (#22)
    🚀 transform pasted content into markdown format

--- components/LiveMarkdown/index.tsx
+++ components/LiveMarkdown/index.tsx
@@ -1,52 +1,85 @@
-import dynamic from "next/dynamic";
-import { ClipboardEvent, useState } from "react";
-import Visibility from "@mui/icons-material/Visibility";
-import TextField from "@mui/material/TextField";
-import styles from './live-markdown.module.scss'
-import CenterLoading, { FullScreenLoader } from "../CenterLoadng";
-import { Box } from "@mui/material";
+import dynamic from 'next/dynamic';
+import { ClipboardEvent, useRef, useState } from 'react';
+import TurndownService from 'turndown';
+import Visibility from '@mui/icons-material/Visibility';
+import { Box, TextField } from '@mui/material';
+import styles from './live-markdown.module.scss';
+import CenterLoading, { FullScreenLoader } from '../CenterLoading';
 
-const Markdown = dynamic(() => import('@/components/Markdown'), { loading: () => <CenterLoading />})
+const Markdown = dynamic(() => import('@/components/Markdown'), { loading: () => <CenterLoading /> });
 
 interface Props {
   markdownInput: string;
-  onChange: (data: string) => void
+  onChange: (data: string) => void;
 }
+
+const asyncReplacement = async function (str: string) {
+  const regex = /!\[(.*?)\]\((.*?)\)/g;
+  const promises: Promise<any>[] = [];
+  const map = new Map();
+  str.replace(regex, (match, alt, src) => {
+    if (!src) {
+      console.warn('no src here');
+      map.set(match, '');
+      return '';
+    }
+    const p = fetch('/api/cloudinary/upload', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ resource: src }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        map.set(
+          match,
+          `![{cloudinary} {${result.width}x${result.height}} {caption: ${alt || ''}}](${result.public_id})`,
+        );
+      })
+      .catch((e) => {
+        console.error(`[Upload Image Failed]: ${e}`);
+        return '';
+      });
+    promises.push(p);
+    return '';
+  });
+  await Promise.all(promises);
+  return str.replace(regex, (match) => map.get(match));
+};
 
 const LiveMarkdown: React.FC<Props> = ({ markdownInput, onChange }) => {
   const [loading, setLoading] = useState(false);
+  const textFieldRef = useRef<HTMLTextAreaElement | null>(null);
   const handlePaste = async (e: ClipboardEvent<HTMLDivElement>) => {
-    const dT = e.clipboardData
-    const items = dT.items;
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].type.indexOf('image') !== -1) {
-        const html = dT.getData('text/html') || "";
-        const parsed = new DOMParser().parseFromString(html, 'text/html');
-        const img = parsed.querySelector('img');
-        if(!img) { console.warn('no image here'); return; }
-        
-        try {
-          setLoading(true)
-          const result = await fetch('/api/cloudinary/upload', {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({resource: img.src}),
-          }).then(res => res.json())
-          onChange(markdownInput + `![{cloudinary} {${result.width}x${result.height}} {caption: ${img.alt}}](${result.public_id})`)
-        } catch (error) {
-          console.error(`[Upload Image Failed]: ${error}`);
-        } finally {
-          setLoading(false)
-        }
-      } else {
-        items[i].getAsString((data) => {
-          onChange(markdownInput + data)
-        });
-      }
+    e.preventDefault();
+    const turndownService = new TurndownService();
+    const dT = e.clipboardData;
+    const html = dT.getData('text/html') || '';
+    const md = turndownService.turndown(html);
+    setLoading(true);
+    try {
+      const result = await asyncReplacement(md);
+      insertText(result);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
+  const insertText = (text: string) => {
+    const ele = textFieldRef.current as HTMLTextAreaElement;
+    // Get the current cursor position
+    const selectionStart = ele.selectionStart;
+    const selectionEnd = ele.selectionEnd;
+
+    // Insert the new text at the current cursor position
+    const newText = ele.value.substring(0, selectionStart) + text + ele.value.substring(selectionEnd);
+    onChange(newText);
+
+    // Set the new cursor position after the newly inserted text
+    ele.setSelectionRange(selectionStart + text.length, selectionStart + text.length);
+  };
   return (
-    <div className={styles["live-markdown__container"]}>
+    <div className={styles['live-markdown__container']}>
       <div className={styles.wrapper}>
         <div className={styles['editor-box']}>
           <div className={styles.head}>
@@ -56,6 +89,7 @@ const LiveMarkdown: React.FC<Props> = ({ markdownInput, onChange }) => {
           <TextField
             id="markdown-content"
             placeholder="Markdown content"
+            inputRef={textFieldRef}
             multiline
             autoFocus
             className={styles.markdown}
@@ -77,6 +111,6 @@ const LiveMarkdown: React.FC<Props> = ({ markdownInput, onChange }) => {
         </div>
       </div>
     </div>
-  )
-}
-export default LiveMarkdown
+  );
+};
+export default LiveMarkdown;
